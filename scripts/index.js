f02ad744cd988f1b121a990f4061783e81c7d1e0
Gleb <151743016+CblRo4Ek@users.noreply.github.com>
2025-05-26 17:02:28 +0000

    Почти готово

--- scripts/index.js
+++ scripts/index.js
@@ -117,14 +117,14 @@ function createCard(item){
     const newItem = template.content.querySelector('.card').cloneNode(true);
     const cardsImg = newItem.querySelector('.card__image');
     const cardsTitle = newItem.querySelector('.card__title')
-    // const cardsBtnRemove = newItem.querySelector('card__delete-button');
+    const cardsBtnRemove = newItem.querySelector('.card__delete-button');
     const cardsLike = newItem.querySelector('.card__like-button');
 
     cardsImg.src = item.link;
     cardsImg.alt = item.name;
     cardsTitle.textContent = item.name;
 
-    // cardsBtnRemove.addEventListener('click', removeItem);
+    cardsBtnRemove.addEventListener('click', removeItem);
 
     cardsLike.addEventListener('click', function (evt) {
         evt.target.classList.toggle('card__like-button_is-active');
